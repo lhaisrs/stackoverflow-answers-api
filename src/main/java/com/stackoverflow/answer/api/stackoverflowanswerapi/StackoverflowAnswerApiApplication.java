a44ef1ca99a29333f89e4bf986b34c01ed23e60f
Lhaís Rodrigues Silva <l.rodrigues.silva@accenture.com>
2021-02-22 21:32:19 -0300

    Initializing project

---
+++ src/main/java/com/stackoverflow/answer/api/stackoverflowanswerapi/StackoverflowAnswerApiApplication.java
@@ -0,0 +1,13 @@
+package com.stackoverflow.answer.api.stackoverflowanswerapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StackoverflowAnswerApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StackoverflowAnswerApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/stackoverflow/answer/api/stackoverflowanswerapi/StackoverflowAnswerApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stackoverflow.answer.api.stackoverflowanswerapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StackoverflowAnswerApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
